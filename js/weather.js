24a3e7524457a28b34cfb84c6cdad00489c4462e
bryanlohjy <bryanlohjy@hotmail.com>
2016-04-29 01:03:06 +1200

    To Extract Info from JSON

--- js/weather.js
+++ js/weather.js
@@ -7,21 +7,30 @@ function currentPosition(position){
 	var lat  = position.coords.latitude;
 	var long = position.coords.longitude;
 
-	$('#lat').html("Latitude: " + lat);
-	$('#long').html("Longitude: " + long);
+	$('#lat').append(lat);
+	$('#long').append(long);
 
 	var url = "https://api.opencagedata.com/geocode/v1/json?q="+lat+"+"+long+"&pretty=1&key=3f48944875604c7fb58939745a8b52bd"
-
-	console.log('lat',lat);
-	console.log('long',long);
-	console.log('url',url);
+	
 
 	$.ajax({
 		type:'GET',
 	    url: url,
 	    dataType: 'json',
-	    success: function(results){
-	        console.log(results);
+	    success: function(location){
+	    	console.log(location);
+
+	    	var state = location["results"];
+	    	console.log(state);
+
+
+
+	    	// $.each(location,function(i,location){
+	     //    	$('#location').append("Location:" + lat);
+
+
+	     //    });
+
 
 	    }
 	});
