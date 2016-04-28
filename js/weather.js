03bfad6b234bc72de806a9f09a3e8c9a72662e13
bryanlohjy <bryanlohjy@hotmail.com>
2016-04-29 01:24:16 +1200

    Extracted info from OpenCage

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@ geolocation = navigator.geolocation;
 geolocation.getCurrentPosition(currentPosition);
 
 
- 
+//Reverse Geocoding : Getting Location Info from current coordinates
 function currentPosition(position){
 	var lat  = position.coords.latitude;
 	var long = position.coords.longitude;
@@ -11,8 +11,8 @@ function currentPosition(position){
 	$('#long').append(long);
 
 	var url = "https://api.opencagedata.com/geocode/v1/json?q="+lat+"+"+long+"&pretty=1&key=3f48944875604c7fb58939745a8b52bd"
-	
-
+		
+	//Extracting state / country code info to get info from WUNDERGROUND API
 	$.ajax({
 		type:'GET',
 	    url: url,
@@ -20,18 +20,12 @@ function currentPosition(position){
 	    success: function(location){
 	    	console.log(location);
 
-	    	var state = location["results"];
+	    	//Personal note - . for objects, [] for arrays
+	    	var state = location.results[0].components.state;
+	    	var country_code = location.results[0].components.country_code;
+	    	
 	    	console.log(state);
-
-
-
-	    	// $.each(location,function(i,location){
-	     //    	$('#location').append("Location:" + lat);
-
-
-	     //    });
-
-
+	    	console.log(country_code);
 	    }
 	});
 
