8a5d56fdd854bc83e9cbb28521912f2e26925889
bryanlohjy <bryanlohjy@hotmail.com>
2016-05-02 01:37:58 +1200

    calculations and status added

--- js/calculations.js
+++ js/calculations.js
@@ -1,46 +1,3 @@
-var rootRef = new Firebase('https://wanderwear.firebaseio.com/');
-
-//writing data
-
-var usersRef = rootRef.child("users");
-
-$( "#feedback-hot" ).click(function() {
-	usersRef.child("user-1").child("feedback").push(-1)
-	
-});
-
-$( "#feedback-good" ).click(function() {
-	usersRef.child("user-1").child("feedback").push(0)
-	
-});
-
-$( "#feedback-cold" ).click(function() {
-	usersRef.child("user-1").child("feedback").push(1)
-	
-});
-
-$( "#clear-bias" ).click(function() {
-	usersRef.child("user-1").child("feedback").set(0)
-	
-});
-
-usersRef.child("user-1").child("feedback").on('value', function(child_snapshot) {
-	
-	var bias_list = child_snapshot.val();
-
-	var bias_array = $.map(bias_list, function(value, index) {
-	    return [value];
-	});
-
-
-	console.log(bias_array);
-	//regex to get value
-	//find total number of objects
-});
-
-
-
-
 // var ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts");
 // // Retrieve new posts as they are added to our database
 // ref.on("child_added", function(snapshot, prevChildKey) {
@@ -81,23 +38,99 @@ var calc_temperature = Number(c_temperature.replace(/[^0-9\.]+/g,""));
 var predicted_comfort = calc_temperature/(sum_CLO*15);
 
 $('#predicted-comfort').html("Predicted Comfort: " + predicted_comfort);
+}
+,500);
 
 
-if(predicted_comfort < 2) {                            
-// $('.slide-outcome').css({'background-color':'#00cc00'});
-$('#comfort-status').css({'background-color':'#00cc00'});
-$('#comfort-status').html("You are good to go.");
-
-}else{
-// $('.slide-outcome').css({'background-color':'red'});
-$('#comfort-status').css({'background-color':'red'});
-$('#comfort-status').html("Try again.");
-}       
 
 
+
+var rootRef = new Firebase('https://wanderwear.firebaseio.com/');
+
+//writing data
+
+var usersRef = rootRef.child("users");
+
+$( "#feedback-hot" ).click(function() {
+	usersRef.child("user-1").child("feedback").push(2)
+	
+});
+
+$( "#feedback-good" ).click(function() {
+	//no changes to bias
+});
+
+$( "#feedback-cold" ).click(function() {
+	usersRef.child("user-1").child("feedback").push(-2)
+	
+});
+
+$( "#clear-bias" ).click(function() {
+	usersRef.child("user-1").child("feedback").set(0)
+	
+});
+
+//turning object list to array of numbers
+usersRef.child("user-1").child("feedback").on('value', function(child_snapshot) {
+	
+	var bias_list = child_snapshot.val();
+
+	var bias_array = $.map(bias_list, function(value, index) {
+	    return [value];
+	});
+
+	var sum = 0;
+	for(var k = 0; k < bias_array.length; k++){
+    	var sum = sum + bias_array[k];
+	}
+
+	var average_bias = sum/bias_array.length;
+	
+	$('#user-bias').html("User Bias: " + average_bias);
+
+	var read_predicted_comfort = document.getElementById('predicted-comfort').innerHTML;
+	var calc_predicted_comfort = Number(read_predicted_comfort.replace(/[^0-9\.]+/g,""));
+	var evaluated_comfort = calc_predicted_comfort-average_bias;
+
+	$('#evaluated-comfort').html("Evaluated Comfort: " + evaluated_comfort);
+
+});
+
+
+setInterval (function statusIndicator(){
+	var read_predicted_comfort = document.getElementById('predicted-comfort').innerHTML;
+	var calc_predicted_comfort = Number(read_predicted_comfort.replace(/[^0-9\.]+/g,""));
+
+	var read_evaluated_comfort = document.getElementById('evaluated-comfort').innerHTML;
+	var calc_evaluated_comfort = Number(read_evaluated_comfort.replace(/[^0-9\.]+/g,""));
+
+	// console.log(calc_evaluated_comfort);
+	// console.log(calc_predicted_comfort);
+
+
+	if(calc_evaluated_comfort == 0) {     
+		if(calc_predicted_comfort < 1) {                            
+		// $('.slide-outcome').css({'background-color':'#00cc00'});
+		$('#comfort-status').css({'background-color':'#00cc00'});
+		$('#comfort-status').html("You are good to go.");
+
+		}else{
+		// $('.slide-outcome').css({'background-color':'red'});
+		$('#comfort-status').css({'background-color':'red'});
+		$('#comfort-status').html("Try again.");
+		}       
+
+
+	}else{
+		if(calc_evaluated_comfort < 1) {                            
+		// $('.slide-outcome').css({'background-color':'#00cc00'});
+		$('#comfort-status').css({'background-color':'#00cc00'});
+		$('#comfort-status').html("You are good to go.");
+
+		}else{
+		// $('.slide-outcome').css({'background-color':'red'});
+		$('#comfort-status').css({'background-color':'red'});
+		$('#comfort-status').html("Try again.");
+		}       
+	}
 },500);
-
-
-
-
-
