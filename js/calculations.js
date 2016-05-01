2c93d0a75e6946ab981d3b40446b907e35c13bfc
bryanlohjy <bryanlohjy@hotmail.com>
2016-05-01 20:33:29 +1200

    Buttons Added

--- js/calculations.js
+++ js/calculations.js
@@ -16,15 +16,20 @@ $('#sum-CLO').html("Sum CLO: " + sum_CLO);
 var c_temperature = document.getElementById('air-temperature').innerHTML;
 var calc_temperature = Number(c_temperature.replace(/[^0-9\.]+/g,""));
 
-var predicted_comfort = calc_temperature/(sum_CLO*10);
+var predicted_comfort = calc_temperature/(sum_CLO*15);
 
 $('#predicted-comfort').html("Predicted Comfort: " + predicted_comfort);
 
 
 if(predicted_comfort < 2) {                            
-$('.slide-outcome').css({'background-color':'#00cc00'});
+// $('.slide-outcome').css({'background-color':'#00cc00'});
+$('#comfort-status').css({'background-color':'#00cc00'});
+$('#comfort-status').html("You are good to go.");
+
 }else{
-$('.slide-outcome').css({'background-color':'red'});
+// $('.slide-outcome').css({'background-color':'red'});
+$('#comfort-status').css({'background-color':'red'});
+$('#comfort-status').html("Try again.");
 }       
 
 
